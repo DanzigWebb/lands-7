583ac1d620815e0239343af812ef8a5ea7ab216d
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-11-01 00:38:15 +0300

    c

---
+++ js/common.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.main-slider', {
+  autoHeight: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  spaceBetween: 40
+});
